a104dda0810ad138a4b7605472d32242d9b8a8a8
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-20 16:14:44 +0000

    If else statement

--- index.js
+++ index.js
@@ -4,12 +4,44 @@ if (x === 9) {
 }else{
   console.log("false");
 }
+console.log("-------");
 //This ends here!
 
 let y = "green";
 if(y == "blue"){
   console.log("blue is detected!");
 }else if(y == "green"){
-  console.log("Green is detected")
-} 
+  console.log("Green is detected!")
+}else{
+  console.log("No Green or Blue Detected!")
+}
+console.log("--------------");
+//These below are some of types of comparison
+let num = 7;
+if(num !== 7){
+  console.log("Not Seven");
+}else{
+  console.log("Luck num Seven!");
+}
+console.log("--------------");
+let numb = 10;
+if(numb > 7){
+  console.log("Greater than Seven");
+}else{
+  console.log("Smaller or equal to seven");
+}
+console.log("--------------");
+let numbe = 10;
+if(numbe < 7){
+  console.log("Greater than Seven");
+}else{
+  console.log("Smaller or equal to seven");
+}
+console.log("--------------");
+let number = 6;
+if(number >= 7){
+  console.log("Greater than or equal to Seven");
+}else{
+  console.log("Smaller");
+}
 
