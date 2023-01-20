4d69df2af0e3eeb5c66b56094072bad4dae67cd3
Kwizera Theo <kwizerasinapy49@gmail.com>
2023-01-20 15:49:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let x = 32;
+if (x === 9) {
+  console.log("True");
+}else{
+  console.log("false");
+}
+//This ends here!
+
+let y = "green";
+if(y == "blue"){
+  console.log("blue is detected!");
+}else if(y == "green"){
+  console.log("Green is detected")
+} 
+
